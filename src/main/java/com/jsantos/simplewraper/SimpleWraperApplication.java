0a933634c6711fe941c77f29383700e271d86a32
Jeferson Santos <jefersonsantos006@hotmail.com>
2026-06-14 14:48:53 -0300

    init

---
+++ src/main/java/com/jsantos/simplewraper/SimpleWraperApplication.java
@@ -0,0 +1,13 @@
+package com.jsantos.simplewraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleWraperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleWraperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jsantos/simplewraper/SimpleWraperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsantos.simplewraper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleWraperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
